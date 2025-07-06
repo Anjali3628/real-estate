75e9b95d9d32bd6ceeec278a3344c95374485427
Anjali Patel <anjalipatel2503@gmail.com>
2025-07-06 18:54:20 +0530

    update file

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,57 @@
 module.exports = function(grunt) {
-    const sass = require('sass'); // Use dart-sass
+    const sass = require('sass'); // using dart-sass
 
     grunt.initConfig({
-        // Sass compilation to dist folder
+        // SCSS to minified CSS
         sass: {
             dist: {
                 options: {
                     outputStyle: 'compressed',
                     implementation: sass,
-                    includePaths: ["node_modules/bootstrap/scss"],
+                    includePaths: ["node_modules/bootstrap/scss"]
                 },
                 files: [{
-                    'dist/assets/css/style.min.css': 'src/assets/scss/styles.scss',
+                    'src/assets/css/style.min.css': 'src/assets/scss/styles.scss'
                 }]
             }
         },
 
-        // Uglify JS to dist folder
+        // JS minification
         uglify: {
             my_target: {
                 files: {
-                    'dist/assets/bundles/libscripts.bundle.js': [
+                    'src/assets/bundles/libscripts.bundle.js': [
                         'node_modules/jquery/dist/jquery.min.js',
-                        'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+                        'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
                     ]
                 }
             }
         },
 
-        // Copy HTML files to dist folder
+        // Copy necessary files to dist/
         copy: {
-            html: {
+            main: {
                 expand: true,
-                cwd: './',
-                src: ['index.html', 'contact.html', 'service.html'],
+                cwd: 'real-estate/',
+                src: [
+                    'index.html',
+                    'contact.html',
+                    'service.html',
+                    'src/assets/css/style.min.css',
+                    'src/assets/bundles/libscripts.bundle.js',
+                    'src/assets/images/**',
+                    'src/assets/fonts/**'
+                ],
                 dest: 'dist/'
             }
         }
     });
 
-    // Load required plugins
-    grunt.loadNpmTasks("grunt-sass");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-copy");
+    // Load required grunt plugins
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    // Define individual tasks
-    grunt.registerTask("buildcss", ["sass"]);
-    grunt.registerTask("buildjs", ["uglify"]);
-    grunt.registerTask("copyhtml", ["copy"]);
-
-    // Master build task for Netlify
-    grunt.registerTask("build", ["buildcss", "buildjs", "copyhtml"]);
+    // Default task
+    grunt.registerTask('default', ['sass', 'uglify', 'copy']);
 };
